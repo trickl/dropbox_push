abc7e21d0118e5e104d8ea6505c51cc6c91c9915
Tim Gee <writetotimgee@gmail.com>
2016-03-11 19:13:18 +0000

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # dropbox_push
 Simple script to automate pushing files onto drop box (one-way) for archival. 
+
+usage dropbox_push.sh --from [from_dir] --to [dropbox_dir] --sync [sync_dir]
+
+(The sync directory can be the same as the from directory, if the from directory is writable. It CANNOT Be the dropbox directory).
+
+WARNING: While this script should be harmless (it doesn't delete files, only excludes directories in Dropbox from syncing) - use at your own risk. 
+I recommend reading the script first to understand what it does. Here is an overview.
+
+It works on a folder by folder basis.
+For each folder in the source directory, it looks for a corresponding folder in the target directory (under Dropbox).
+
+If the folder does not exist in dropbox, or the folder does exist and isn't marked as "uploaded" then it should be synchronized.
+   a The folder is removed from the Dropbox folder exclusion list (if it was in it).
+   b) The folder is created if necessary.
+   c) The contents from the --from subdirectory are copied over.
+   d) The script waits for dropbox to complete syncing.
+   e) Once complete, the folder is added to the Dropbox folder exclusion list. Note this removes if from the local file system, but not Dropbox. It also still exists in the --from location.
+   f) The directory is marked as "uploaded".
+
+The script continues until no folders are left.
+
+KEEPING TRACK:
+The sync directory can be the same as the source directory, or a completely different tree (if the source is read only)
+Note it cannot be the target directory withinDropbox, because the target directory is removed from the local file system when it is excluded
+The sync directory just keeps track of which folders have been uploaded already, by placing a ".uploaded" file in the corresponding relative directory.
---
+++ dropbox_push.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+# This script should perform a one-way synchronisation with Dropbox
+
+while [[ $# > 1 ]]
+do
+key="$1"
+
+case $key in
+    -f|--from)
+    FROM_DIR="$2"
+    shift # past argument
+    ;;
+    -t|--to)
+    TO_DIR="$2"
+    shift # past argument
+    ;;
+    -s|--sync)
+    SYNC_DIR="$2"
+    shift # past argument
+    ;;
+    *)
+    # unknown option
+    echo "Usage: $0 --from [source_directory] --to [dropbox_directory] --sync [sync_directory]"
+    exit
+    ;;
+esac
+shift # past argument or value
+done
+
+if [[ ! -d $FROM_DIR ]] 
+then
+   echo --from $FROM_DIR does not exist
+   exit
+fi
+
+if [[ ! -d $TO_DIR ]] 
+then
+   echo --to $TO_DIR does not exist
+   exit
+fi
+
+if [[ ! -d $SYNC_DIR ]] 
+then
+   echo --sync $SYNC_DIR does not exist
+   exit
+fi
+
+# Convert to absolute directories
+FROM_DIR="$( cd "$FROM_DIR" && pwd )"
+TO_DIR="$( cd $TO_DIR && pwd )"
+SYNC_DIR="$( cd "$SYNC_DIR" && pwd )"
+
+echo Syncing Dropbox...
+echo From "${FROM_DIR}"
+echo To   "${TO_DIR}"
+
+function wait_for_dropbox() {
+  until `dropbox status| grep -q "Up to date"`; do
+    sleep 1
+  done
+}
+
+pushd ${FROM_DIR} > /dev/null
+
+   for FROM_SUB_DIR in `find . -mindepth 1 -type d`
+   do
+      # TODO order the processing according to age of folder?
+      TO_SUB_DIR=`echo ${TO_DIR}/${FROM_SUB_DIR}`
+      SYNC_SUB_DIR=`echo ${SYNC_DIR}/${FROM_SUB_DIR}`
+      echo -n "Synchronising ${FROM_SUB_DIR}... "
+
+      [[ -d ${TO_SUB_DIR} ]] || mkdir -p ${TO_SUB_DIR}
+      [[ -d ${SYNC_SUB_DIR} ]] || mkdir -p ${SYNC_SUB_DIR}
+
+      if [[ ! -f ${SYNC_SUB_DIR}/.uploaded ]]
+      then
+         # Allow synchronisation (allows users to manually remove the marker flag to force a resync).
+         dropbox exclude remove ${TO_SUB_DIR}
+
+         # Wait for dropbox
+         wait_for_dropbox
+         
+         # Copy files
+         pushd ${FROM_SUB_DIR} /dev/null
+         $( find . -mindepth 1 -maxdepth 1 -type f -exec cp \{\} ${TO_SUB_DIR}/{} \; )
+
+         # Wait for dropbox
+         wait_for_dropbox
+
+         # Stop synchronisation
+         dropbox exclude add ${TO_SUB_DIR}
+
+         touch ${SYNC_SUB_DIR}/.uploaded
+
+         popd > /dev/null
+         
+      fi
+
+      echo "completed."
+   done
+popd > /dev/null
+
